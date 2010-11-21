d9a927bf7acdcc17b1e3152f5187975f592dce12
Sidra <github@ambt.us>
2010-11-21 12:36:44 -0500

    rails new tdd_clean_start -O -T -J

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddCleanStart::Application.config.secret_token = '7fea79be156cc2221d894dc84cdd39e15283c1ce3924d914722b7aeff742dd6b77f91d69d8f9c2835b413f11a2c1bad2374d14c85cff82c48a16fe64b618e4a8'
